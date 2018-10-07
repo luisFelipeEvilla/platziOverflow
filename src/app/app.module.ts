0d1ebf46fe0ba1f2a5ec5585181910ffcb0ed83e
Luis Felipe <luisfelipe588@hotmail.com>
2018-10-06 21:56:24 -0500

    first design of the header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import 'hammerjs';
+
+import { QuestionDetailComponent } from './questions/question.detail.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
